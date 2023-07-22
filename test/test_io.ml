a3907114e5552f906a8c7f2e94e491ce76c508ca
Sam Mercer <smercer10@proton.me>
2023-07-22 11:34:39 +0100

    test(io): add unit test for empty read/write

--- test/test_io.ml
+++ test/test_io.ml
@@ -1,11 +1,21 @@
 open Ocrypt
 
 let%expect_test "Text file read/write" =
-  let file = "test.txt" in
+  let file = "hello_world.txt" in
   let write_str = "Hello World!" in
   let buffer = Bytes.of_string write_str in
   Io.write_file file buffer;
   let read_str = Bytes.to_string (Io.read_file file) in
   print_endline read_str;
   [%expect {| Hello World! |}]
+;;
+
+let%expect_test "Empty read/write" =
+  let file = "empty.txt" in
+  let write_str = "" in
+  let buffer = Bytes.of_string write_str in
+  Io.write_file file buffer;
+  let read_str = Bytes.to_string (Io.read_file file) in
+  print_endline read_str;
+  [%expect {| |}]
 ;;
